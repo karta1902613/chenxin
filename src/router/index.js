cc18caf3c4de8026f9362b7e3154def76f7229e6
kevin <karta1902613@gmail.com>
2023-07-29 17:21:18 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,8 @@ import ElProgressBars from "../layouts/sections/elements/progress-bars/ProgressB
 import ElToggles from "../layouts/sections/elements/toggles/TogglesView.vue";
 import ElTypography from "../layouts/sections/elements/typography/TypographyView.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('chenxin2023'),
   routes: [
     {
       path: "/",
